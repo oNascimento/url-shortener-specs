8e706ec3a56a4cb1f1db5748f5e70cba31ad2380
Otavio Nascimento <15211983+oNascimento@users.noreply.github.com>
2026-09-10 09:33:43 -0300

    fix(tests): remove unused link contract import

--- tests/Shortener.LinkManagement.Tests/HttpTests.cs
+++ tests/Shortener.LinkManagement.Tests/HttpTests.cs
@@ -4,7 +4,6 @@ using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Shortener.Application;
 using Xunit;
 
 namespace Shortener.LinkManagement.Tests;
